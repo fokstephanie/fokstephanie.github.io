409d4070ddef1285889e4ecc35e27ded5416789d
Stephanie Fok <s3fok@edu.uwaterloo.ca>
2018-02-14 00:49:25 -0500

    Added opacity fading to work-boxes

--- index.js
+++ index.js
@@ -11,12 +11,29 @@ $(document).ready(function(){
     }, 1000);
     return false;
   });
-/*
-  $(".work-box").hover(function() {
-    var title = $('.word-box > ');
-    title.fadeToggle();
-    var desc = $(this).querySelector("p.desc");
-    desc.fadeToggle();
-  })*/
+
+  $(".waton-desc").hover(function() {
+     $('.waton-desc').toggleClass("work-box-hover");
+     $('.waton-desc > p.box-title').toggleClass("title-hover");
+     $('.waton-desc > p.desc').toggle();
+  });
+
+  $(".quadris-desc").hover(function() {
+     $('.quadris-desc').toggleClass("work-box-small-hover");
+     $('.quadris-desc > p.box-title').toggleClass("title-hover");
+     $('.quadris-desc > p.desc').toggle();
+  });
+
+  $(".aspire-desc").hover(function() {
+     $('.aspire-desc').toggleClass("work-box-small-hover");
+     $('.aspire-desc > p.box-title').toggleClass("title-hover");
+     $('.aspire-desc > p.desc').toggle();
+   });  
+
+  $(".www-desc").hover(function() {
+     $('.www-desc').toggleClass("work-box-hover");
+     $('.www-desc > p.box-title').toggleClass("title-hover");
+     $('.www-desc > p.desc').toggle();
+  });
 });
 
